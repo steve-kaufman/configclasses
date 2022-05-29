aded0dc217597b85bbc1a114bf0947e1d286cb71
Steven Kaufman <Steven.Kaufman@gtt.com>
2022-05-29 16:18:24 -0500

    removed mypy cache; enhanced example file

--- examples/my_config_class.py
+++ examples/my_config_class.py
@@ -1,11 +1,12 @@
 from configclasses import configclass
+from configclasses.configtype import cfgtype
 
 @configclass
 class MyConfigClass:
     """Check out the docstring in the CLI's help output!"""
-    foo: str
-    bar: int
-    baz: bool
+    foo: cfgtype(str, help="input foo")
+    bar: cfgtype(int, help="how many bar to use")
+    baz: cfgtype(bool, cli_args=['-b', '--use-baz'], help="enable baz")
 
 if __name__ == "__main__":
     MyConfigClass()
